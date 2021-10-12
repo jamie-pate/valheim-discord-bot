0ca1750199ceb39d2e4ce086de544732f4191f16
bayoumaroon <49701150+bayoumaroon@users.noreply.github.com>
2021-10-12 15:03:30 -0500

    Update _logsubprocess.py
    Added fixes noted in ckbaudio issues #8, thanks @spud11

--- code/_logsubprocess.py
+++ code/_logsubprocess.py
@@ -1,10 +1,15 @@
 from datetime import datetime
-import time, os, re
+from socket import timeout
+import time, os, re, a2s
 import csv, asyncio
-from valve.source.a2s import ServerQuerier, NoResponseError
 import config
 
-pdeath = '.*?Got character ZDOID from (\w+) : 0:0'
+## Added new regex define to pickup spaces, if character names have additional characters and are not being picked up add them inside brackets i.e. [\w ']+
+## regex101.com will assist with making sure names are parsed correctly
+## use pdeath inside quotes as search string
+## use '10/12/2021 01:14:24: Got character ZDOID from Example : 0:0' as search string
+
+pdeath = '.*?Got character ZDOID from ([\w ]+) : 0:0'
 log = config.file
 
 async def timenow():
@@ -15,13 +20,13 @@ async def timenow():
 async def writecsv():
     while True:    
         try:
-            with ServerQuerier(config.SERVER_ADDRESS) as server:
-                with open('csv/playerstats.csv', 'a', newline='') as f:
-                    csvup = csv.writer(f, delimiter=',')  
-                    curtime, players = await timenow(), server.info()['player_count']
-                    csvup.writerow([curtime, players])
-                    print(curtime, players)
-        except NoResponseError:
+            server = a2s.info(config.SERVER_ADDRESS)
+            with open('csv/playerstats.csv', 'a', newline='') as f:
+                csvup = csv.writer(f, delimiter=',')
+                curtime, players = await timenow(), server.player_count
+                csvup.writerow([curtime, players])
+                print(curtime, players)
+        except timeout:
             with open('csv/playerstats.csv', 'a', newline='') as f:
                 csvup = csv.writer(f, delimiter=',')  
                 curtime, players = await timenow(), '0'
